310988472c9c92f1a679509416f966b28200d403
joshtosh20 <99664034+joshtosh20@users.noreply.github.com>
2022-03-02 22:53:05 +0300

    Add files via upload

---
+++ LM.cpp2.cpp
@@ -0,0 +1,29 @@
+/*
+Typical library management system
+by Joshua
+Feb 2022
+MIT license
+c99 compiler
+*/
+# include <stdio.h>
+# include <stdio.h>
+
+int main()
+{
+	printf("select an action below\n");
+	printf("what would you like to do?\n");
+	printf("1.Add new manager\n");
+	printf("2.view all managers\n";
+	printf("3.Add new resource\n");
+	printf("4.view all resources\n");
+	printf("your action");
+	scanf("%d",action);
+}
+int main ()	
+{
+	printf("county library system\n");
+	printf("welcome Mr Joshua\n");
+	action=menu();
+	printf("you selected action %d\n",action);
+	return 0;
+	}
